4f49cbe16a5ba4a64c5d8b2ef691658dcfcc9b5b
Naftoreiclag <naftoreiclag@gmail.com>
2014-07-07 12:59:56 -0700

    hegrawfqerht

--- src/me/naftoreiclag/test/AreaMakerTest.java
+++ src/me/naftoreiclag/test/AreaMakerTest.java
@@ -1,13 +1,20 @@
 package me.naftoreiclag.test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
+import javax.swing.UIManager;
 
 @SuppressWarnings("serial")
 public class AreaMakerTest extends JFrame
 {
+	AreaMakerTestPanel panel;
+	
 	private AreaMakerTest() throws Exception
 	{
 		super("Pig Collision Demo");
@@ -15,22 +22,70 @@ public class AreaMakerTest extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(500, 500);
 		this.setLocationRelativeTo(null);
+		
+		panel = new AreaMakerTestPanel();
 
 		//
 		
 		JMenuBar menuBar = new JMenuBar();
 
-		JMenu menu = new JMenu("File");
-		menuBar.add(menu);
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
 
-		JMenu menu2 = new JMenu("About");
-		menuBar.add(menu2);
+		JMenuItem fileMenuNew = new JMenuItem("New");
+		fileMenuNew.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileNew(e);
+			}
+		});
+		fileMenu.add(fileMenuNew);
+
+		JMenuItem fileMenuCFI = new JMenuItem("Create from image");
+		fileMenuCFI.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileCFIPressed(e);
+			}
+		});
+		fileMenu.add(fileMenuCFI);
+
+		JMenuItem fileMenuOpen = new JMenuItem("Open");
+		fileMenuOpen.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileOpen(e);
+			}
+		});
+		fileMenu.add(fileMenuOpen);
+
+		JMenuItem fileMenuSave = new JMenuItem("Save");
+		fileMenuSave.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileSave(e);
+			}
+		});
+		fileMenu.add(fileMenuSave);
+
+		JMenu editMenu = new JMenu("Edit");
+		menuBar.add(editMenu);
+
+		JMenu aboutMenu = new JMenu("About");
+		menuBar.add(aboutMenu);
 
 		this.setJMenuBar(menuBar);
 
 		//
 		
-		AreaMakerTestPanel panel = new AreaMakerTestPanel();
 		JScrollPane scrollContainer = new JScrollPane(panel);
 
 		this.add(scrollContainer);
@@ -38,6 +93,14 @@ public class AreaMakerTest extends JFrame
 
 	public static void main(String[] args) throws Exception
 	{
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(Exception e)
+		{
+		}
+		
 		AreaMakerTest m = new AreaMakerTest();
 		m.setVisible(true);
 	}
--- src/me/naftoreiclag/test/AreaMakerTestPanel.java
+++ src/me/naftoreiclag/test/AreaMakerTestPanel.java
@@ -1,17 +1,56 @@
 package me.naftoreiclag.test;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class AreaMakerTestPanel extends JPanel
 {
+JFileChooser fileChooser = new JFileChooser();
+	
 	public AreaMakerTestPanel() throws Exception
 	{
 		this.setSize(500, 500);
 		
 		this.setFocusable(true);
 		this.requestFocusInWindow();
+	}
+
+	public void onFileNew(ActionEvent e)
+	{
+		// TODO Auto-generated method stub
 		
+	}
+
+	public void onFileCFIPressed(ActionEvent e)
+	{
+		int returnVal = fileChooser.showOpenDialog(this);
+
+		if (returnVal == JFileChooser.APPROVE_OPTION)
+		{
+			File file = fileChooser.getSelectedFile();
+			System.out.println(file.getName());
+		}
+		else
+		{
+			System.out.println("closed");
+		}
+	}
+
+	public void onFileOpen(ActionEvent e)
+	{
+		
+	}
+
+	public void onFileSave(ActionEvent e)
+	{
+		// TODO Auto-generated method stub
 		
 	}
 }
