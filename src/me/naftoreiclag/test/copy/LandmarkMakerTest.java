00e8318662fef9ede9f9a4d61e66d6ae88b99229
Naftoreiclag <naftoreiclag@gmail.com>
2014-07-07 22:40:31 -0700

    hwaaegregthger

---
+++ src/me/naftoreiclag/test/copy/LandmarkMakerTest.java
@@ -0,0 +1,108 @@
+package me.naftoreiclag.test.copy;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.UIManager;
+
+@SuppressWarnings("serial")
+public class LandmarkMakerTest extends JFrame
+{
+	LandmarkMakerTestPanel panel;
+	
+	private LandmarkMakerTest() throws Exception
+	{
+		super("Pig Collision Demo");
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500, 500);
+		this.setLocationRelativeTo(null);
+		
+		panel = new LandmarkMakerTestPanel();
+
+		//
+		
+		JMenuBar menuBar = new JMenuBar();
+
+		JMenu fileMenu = new JMenu("File");
+		menuBar.add(fileMenu);
+
+		JMenuItem fileMenuNew = new JMenuItem("New");
+		fileMenuNew.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileNew(e);
+			}
+		});
+		fileMenu.add(fileMenuNew);
+
+		JMenuItem fileMenuCFI = new JMenuItem("Create from image");
+		fileMenuCFI.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileCFIPressed(e);
+			}
+		});
+		fileMenu.add(fileMenuCFI);
+
+		JMenuItem fileMenuOpen = new JMenuItem("Open");
+		fileMenuOpen.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileOpen(e);
+			}
+		});
+		fileMenu.add(fileMenuOpen);
+
+		JMenuItem fileMenuSave = new JMenuItem("Save");
+		fileMenuSave.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				panel.onFileSave(e);
+			}
+		});
+		fileMenu.add(fileMenuSave);
+
+		JMenu editMenu = new JMenu("Edit");
+		menuBar.add(editMenu);
+
+		JMenu aboutMenu = new JMenu("About");
+		menuBar.add(aboutMenu);
+
+		this.setJMenuBar(menuBar);
+
+		//
+		
+		JScrollPane scrollContainer = new JScrollPane(panel);
+		panel.giveScrolPaneAccess(scrollContainer);
+
+		this.add(scrollContainer);
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(Exception e)
+		{
+		}
+		
+		LandmarkMakerTest m = new LandmarkMakerTest();
+		m.setVisible(true);
+	}
+}
---
+++ src/me/naftoreiclag/test/copy/LoadedLandmarkProject.java
@@ -0,0 +1,131 @@
+package me.naftoreiclag.test.copy;
+
+import java.awt.image.BufferedImage;
+
+public class LoadedLandmarkProject
+{
+	byte[][] pixelData;
+	boolean[][] collisionData;
+
+	BufferedImage displayImage;
+	
+	int cWidth;
+	int cHeight;
+	
+	int tWidth;
+	int tHeight;
+	
+	int pWidth;
+	int pHeight;
+	
+	public static final int[] pallete = {0x000000, 0x333333, 0x555555, 0x777777, 0x999999, 0xBBBBBB, 0xDDDDDD, 0xFFFFFF};
+
+	public void setCollision(int x, int y, boolean yesNo)
+	{
+		//wats
+		collisionData[x][y] = yesNo;
+	}
+	
+	
+	public LoadedLandmarkProject(BufferedImage image)
+	{
+		
+		cWidth = image.getWidth() >> 7;
+		cHeight = image.getHeight() >> 7;
+		System.out.println("New projected loaded with " + cWidth + " by " + cHeight);
+		tWidth = cWidth << 4;
+		tHeight = cHeight << 4;
+		collisionData = new boolean[tWidth][tHeight];
+		System.out.println("New projected loaded with " + tWidth + " by " + tHeight);
+		pWidth = cWidth << 7;
+		pHeight = cHeight << 7;
+		System.out.println("New projected loaded with " + pWidth + " by " + pHeight);
+		
+		pixelData = new byte[pWidth][pHeight];
+
+		for(int x = 0; x < pWidth; ++x)
+		{
+			for(int y = 0; y < pHeight; ++y)
+			{
+				pixelData[x][y] = getByteFromRGB(image.getRGB(x, y));
+			}
+		}
+		
+		displayImage = new BufferedImage(pWidth, pHeight, BufferedImage.TYPE_INT_ARGB);
+		
+		
+		for(int x = 0; x < pWidth; ++x)
+		{
+			for(int y = 0; y < pHeight; ++y)
+			{
+				displayImage.setRGB(x, y, pallete[pixelData[x][y]] | 0xFF000000);
+			}
+		}
+	}
+	
+	/*
+	public void updateImage(BufferedImage image)
+	{
+		pixelData = new byte[pWidth][pHeight];
+
+		for(int x = 0; x < pWidth; ++x)
+		{
+			for(int y = 0; y < pHeight; ++y)
+			{
+				pixelData[x][y] = getByteFromRGB(image.getRGB(x, y));
+			}
+		}
+		
+		displayImage = new BufferedImage(pWidth, pHeight, BufferedImage.TYPE_INT_ARGB);
+		
+		
+		for(int x = 0; x < pWidth; ++x)
+		{
+			for(int y = 0; y < pHeight; ++y)
+			{
+				displayImage.setRGB(x, y, pallete[pixelData[x][y]]);
+			}
+		}
+	}
+	*/
+
+	public static byte getByteFromRGB(int rgb)
+	{
+		if ((rgb & 0xFF000000) == 0) { return 8; }
+
+		rgb = rgb & 0x00FFFFFF;
+
+		if (rgb < pallete[1])
+		{
+			return 0;
+		}
+		else if (rgb < pallete[2])
+		{
+			return 1;
+		}
+		else if (rgb < pallete[3])
+		{
+			return 2;
+		}
+		else if (rgb < pallete[4])
+		{
+			return 3;
+		}
+		else if (rgb < pallete[5])
+		{
+			return 4;
+		}
+		else if (rgb < pallete[6])
+		{
+			return 5;
+		}
+		else if (rgb < pallete[7])
+		{
+			return 6;
+		}
+		else
+		{
+			return 7;
+		}
+	}
+}
